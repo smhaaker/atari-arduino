192bf6a4460ddbd1bae7e7361eeabfc574c59824
Steffen <steffen.haaker@gmail.com>
2020-08-12 22:34:57 -0400

    cleaned

--- nodesample/index.js
+++ nodesample/index.js
@@ -6,7 +6,8 @@ let location = 0
 
 board.on('ready', function () {
 
-  let button = new five.Button({ pin: 3, isPullup: true })
+  // Define buttons, not as collection since we might want to change holdtime
+  let button = new five.Button({ pin: 3, isPullup: true, holdtime: 100,})
   let up = new five.Button({ pin: 6, isPullup: true })
   let down = new five.Button({ pin: 7, isPullup: true })
   let left = new five.Button({ pin: 4, isPullup: true })
@@ -15,11 +16,14 @@ board.on('ready', function () {
   button.on('down', function () { message('FIRE')})
   button.on('hold', function () { message('hold')})
   button.on('up', function () { message('No FIRE') });
-  up.on('down', function () { location++, console.log('Up'), console.log(location) })
-  down.on('down', function () { location--, console.log('Down'), console.log(location) })
-  left.on('down', function () { console.log('Left') })
-  right.on('down', function () { console.log('Right') })
 
+  up.on('down', function () { location++, console.log('Up'), console.log(location) })
+
+  down.on('down', function () { location--, console.log('Down'), console.log(location) })
+
+  left.on('down', function () { console.log('Left') })
+
+  right.on('down', function () { console.log('Right') })
 });
 
 const message = (value) => {
